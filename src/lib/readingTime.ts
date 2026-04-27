cd807ae332bb80c0c7a06ed0b6edc1e409baa38e
Your Name <david_hibbitts@outlook.com>
2026-04-27 14:26:36 +0100

    feat: add reading time estimation and copy-to-clipboard for code blocks

---
+++ src/lib/readingTime.ts
@@ -0,0 +1,7 @@
+export default function getReadingTime(content: string) {
+  const wordsPerMinute = 200;
+  const noOfWords = content.split(/\s+/g).length;
+  const minutes = noOfWords / wordsPerMinute;
+  const readTime = Math.ceil(minutes);
+  return `${readTime} min read`;
+}
